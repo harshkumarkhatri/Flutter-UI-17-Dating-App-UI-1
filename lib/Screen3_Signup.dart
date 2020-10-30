0a452667c0cce05e276ac649fdd798bad4ee2175
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-10-30 11:12:10 +0530

    Sign up UI added

---
+++ lib/Screen3_Signup.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_17_dating_app_ui_1/Screen2_SignIn.dart';
+import 'package:flutter_ui_17_dating_app_ui_1/Screen4_HomePage.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Screen3 extends StatefulWidget {
+  @override
+  _Screen3State createState() => _Screen3State();
+}
+
+class _Screen3State extends State<Screen3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        // color: Colors.grey,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.center,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.grey,
+              Colors.grey.withOpacity(0.8),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15, right: 15),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => Screen4(),
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.width / 3.1,
+                        bottom: MediaQuery.of(context).size.width / 4.0),
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.w300,
+                          letterSpacing: 1.5),
+                    ),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    hintText: "Username",
+                    hintStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 1.2,
+                        fontSize: 16),
+                    prefixIcon:
+                        FaIcon(FontAwesomeIcons.user, color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 25),
+                TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    hintText: "Email",
+                    hintStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 1.2,
+                        fontSize: 16),
+                    prefixIcon:
+                        FaIcon(FontAwesomeIcons.mailBulk, color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 25),
+                TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    hintText: "Password",
+                    hintStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 1.2,
+                        fontSize: 16),
+                    prefixIcon:
+                        FaIcon(FontAwesomeIcons.lock, color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 25),
+                TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    hintText: "Confirm Password",
+                    hintStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 1.2,
+                        fontSize: 16),
+                    prefixIcon:
+                        FaIcon(FontAwesomeIcons.lock, color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 35),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 55,
+                  // color: Colors.red,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey[600],
+                          blurRadius: 2,
+                          spreadRadius: 0.5)
+                    ],
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.pink[600],
+                        Colors.pink[400],
+                        Colors.orangeAccent[700]
+                      ],
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        letterSpacing: 1.2,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 25),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(text: 'Already a Member? '),
+                      TextSpan(
+                        style: TextStyle(decoration: TextDecoration.underline),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (_) => Screen2(),
+                              ),
+                            );
+                          },
+                        text: 'Sign in',
+                      ),
+                    ],
+                  ),
+                ),
+                // Text(
+                //   "Already a Member? Sign in",
+                //   style: TextStyle(
+                //     color: Colors.white,
+                //     fontWeight: FontWeight.w300,
+                //     letterSpacing: 1.2,
+                //     fontSize: 14,
+                //   ),
+                // ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
