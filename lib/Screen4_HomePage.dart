91959b961faf0393a0148b643fd9aa99db6a368b
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-10-31 11:48:09 +0530

    Drawer is now functional

---
+++ lib/Screen4_HomePage.dart
@@ -0,0 +1,256 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Screen4 extends StatefulWidget {
+  @override
+  _Screen4State createState() => _Screen4State();
+}
+
+class _Screen4State extends State<Screen4> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavDrawer(),
+      appBar: AppBar(
+        // backgroundColor: Colors.pink[600],
+        centerTitle: true,
+        title: Text("Unlock"),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.pink[600],
+                Colors.pink[400],
+                Colors.orangeAccent[700],
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: handleClick,
+            itemBuilder: (BuildContext context) {
+              return {'Logout', 'Settings'}.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          ),
+        ],
+      ),
+      body: Container(
+          child: CustomScrollView(
+        slivers: [
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [],
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+
+  void handleClick(String value) {
+    switch (value) {
+      case 'Logout':
+        break;
+      case 'Settings':
+        break;
+    }
+  }
+}
+
+// Navdrawert class
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        // color: Colors.pink,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Colors.pink[600],
+            Colors.pink[400],
+            Colors.orangeAccent[700]
+          ]),
+        ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            // Container(
+            //   height: 120,
+            //   // color: Colors.pink,
+            //   padding: EdgeInsets.all(12),
+            //   child: Text(
+            //     "Sushy",
+            //     style: TextStyle(
+            //       letterSpacing: 1.4,
+            //       color: Colors.white,
+            //       fontSize: 32,
+            //       fontWeight: FontWeight.w400,
+            //     ),
+            //   ),
+            //   alignment: Alignment.bottomLeft,
+            // ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, top: 15),
+                child: Container(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 64,
+                      width: 64,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Daisy Sparks",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 24,
+                        letterSpacing: 1.2,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "New your city",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                )),
+              ),
+            ),
+            SizedBox(height: 25),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: FaIcon(FontAwesomeIcons.user, color: Colors.white),
+                title: Text(
+                  'User Profile',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: FaIcon(FontAwesomeIcons.cog, color: Colors.white),
+                title: Text(
+                  'Settings',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(FontAwesomeIcons.envelope, color: Colors.white),
+                title: Text(
+                  'Messages',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading:
+                    Icon(FontAwesomeIcons.userFriends, color: Colors.white),
+                title: Text(
+                  'Friends',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(FontAwesomeIcons.bell, color: Colors.white),
+                title: Text(
+                  'Notifications',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading:
+                    Icon(FontAwesomeIcons.calendarAlt, color: Colors.white),
+                title: Text(
+                  'Subscription',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(FontAwesomeIcons.signOutAlt, color: Colors.white),
+                title: Text(
+                  'Sign Out',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
