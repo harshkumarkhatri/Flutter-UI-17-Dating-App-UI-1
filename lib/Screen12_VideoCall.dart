c83245557d1de7568ecc18e8c82bceb69b2e3f49
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-11-02 21:18:55 +0530

    VideoCall screen ui completed, Drawer to be updated

---
+++ lib/Screen12_VideoCall.dart
@@ -0,0 +1,146 @@
+// This is the unlock screen where the fingerprint is made
+
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_17_dating_app_ui_1/Screen6_userProfile.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'NavDrawer.dart';
+
+class Screen12 extends StatefulWidget {
+  @override
+  _Screen12State createState() => _Screen12State();
+}
+
+class _Screen12State extends State<Screen12> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavDrawer(),
+      appBar: AppBar(
+        // backgroundColor: Colors.pink[600],
+        centerTitle: true,
+        title: Text("Video Call"),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.pink[600],
+                Colors.pink[400],
+                Colors.orangeAccent[700],
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: handleClick,
+            itemBuilder: (BuildContext context) {
+              return {'Logout', 'Settings'}.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          ),
+        ],
+      ),
+      body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.grey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "Clifford Hampton",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(height: 15),
+              Text(
+                "New York, USA",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 40, top: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 110,
+                      height: 45,
+                      // color: Colors.red,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey[600],
+                              blurRadius: 2,
+                              spreadRadius: 0.5)
+                        ],
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.pink[600],
+                            Colors.pink[400],
+                            Colors.orangeAccent[700]
+                          ],
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Accept",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            letterSpacing: 1.2,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 45,
+                      width: 110,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2,
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Reject",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            letterSpacing: 1.2,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )),
+    );
+  }
+
+  void handleClick(String value) {
+    switch (value) {
+      case 'Logout':
+        break;
+      case 'Settings':
+        break;
+    }
+  }
+}
