00bbf5be49124568d81d6d8f48021962ccc1f574
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-11-04 17:36:21 +0530

    Message items added

---
+++ lib/Widgets/messageItems.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+Widget messageItem(){
+  return Container(
+            height: 50,
+            // color: Colors.amber[600],
+            child: Row(
+              children: [
+                Stack(
+                  alignment: Alignment.topRight,
+                  children: [
+                    Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      height: 12,
+                      width: 12,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white54),
+                        color: Colors.green,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Alta Ryan",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            "03:57AM",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height:3),
+                      Text(
+                        'your long text hereThis is a long texnijnij nin in inijn njn jknio niojmn iuont',
+                        overflow: TextOverflow.fade,
+                        maxLines: 1,
+                        softWrap: false,
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+}
