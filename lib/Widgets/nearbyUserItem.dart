76882d737595f027884d21eebe87b7fbea395793
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-11-06 09:38:24 +0530

    Added widget for Screen 17

---
+++ lib/Widgets/nearbyUserItem.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+Widget nearbyUserITem(name, distance) {
+  return Container(
+    height: 50,
+    // color: Colors.amber[600],
+    child: Row(
+      children: [
+        Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            shape: BoxShape.circle,
+          ),
+        ),
+        SizedBox(width: 10),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Expanded(
+                  child: Text(
+                    name,
+                    maxLines: 4,
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 3),
+              Text(
+                distance,
+                overflow: TextOverflow.fade,
+                maxLines: 1,
+                softWrap: false,
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                ),
+              )
+            ],
+          ),
+        ),
+        // Spacer(),
+        Icon(
+          Icons.add,
+          color: Colors.grey,
+          size: 35,
+        )
+      ],
+    ),
+  );
+}
